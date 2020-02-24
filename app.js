ff9cd5374eff260c2863cb88ddf429228d54978d
hongwei <hongwei@tesobe.com>
2020-02-24 21:13:00 +0100

    added some comments

--- app.js
+++ app.js
@@ -9,14 +9,19 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// set 方法用于内部变量
+//  views-视图存放目录
 app.set('views', path.join(__dirname, 'views'));
+// view engine setup, 网页模板引擎
 app.set('view engine', 'pug');
 
+// use 用于调用 express 的中间件
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+//设定静态文件目录:
+//public/images/1.png --> http://localhost:3000/images/1.png  网址直接对应的.
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get(
+    '/',  //1st: 路劲
+    function(req, res, next) {
+      var body = 'Hello World';
+      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('Content-Length', body.length);
+      res.end(body);
+      // res.render('index', { title: 'Express' });
+    } //2rd: 回调函数.
+);
 
 module.exports = router;
